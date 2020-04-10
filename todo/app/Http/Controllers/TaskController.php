faa9be2aded9215146b4c0b017455d2fb81aa785
tkmini <tkmini@192.168.3.3>
2020-04-10 17:10:32 +0900

    テンプレート作成 - タスク一覧画面

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     // index
-    public function index() {
-        return "Hello world";
+    public function index(int $id) {
+
+        $folders = Folder::all();
+
+        return view('tasks/index',
+            ['folders' => $folders,
+                'current_folder_id' => $id,]);
+//         return "Hello world";
     }
 }
