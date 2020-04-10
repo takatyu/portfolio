bf4ff060a4477bbc79f101e813ae5677ebfea155
tkmini <tkmini@192.168.3.3>
2020-04-10 15:46:17 +0900

    シーダー - テストデータ作成

---
+++ todo/database/seeds/FoldersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+            ]);
+        }
+    }
+}
