a0dc620c5f1749eca0d3b367e44849659a5bf9b0
tkmini <tkmini@192.168.3.3>
2020-04-11 16:25:51 +0900

    チュートリアル4

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Folder;
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
     // index
     public function index(int $id) {
-
+        // 全てのフォルダデータを取得
         $folders = Folder::all();
 
-        return view('tasks/index',
-            ['folders' => $folders,
-                'current_folder_id' => $id,]);
-//         return "Hello world";
+        // 選ばられたフォルダを取得
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得
+//         $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
     }
 }
